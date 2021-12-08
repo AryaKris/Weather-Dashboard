6d9a2fa28bc46c92d78c6d0cfbe9463936067e24
Arya Krishna <dr.aryakris@outlook.com>
2021-12-07 19:04:51 -0800

    search button

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Weather-Dashboard
 In order  to see the weather outlook for multiple cities building a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS. Here the OpenWeather One Call API is used to retrieve weather data for cities. 
+
+# HTML and CSS
+Since there was no starter code for this assignment the first step was to create an HTML file and do the basic CSS styling. 
--- javascript.js
+++ javascript.js
@@ -1,13 +1,34 @@
-function geoData(lat, lon){
-var url = `http://` ;
-fetch (url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-
-
-});
-
-
+function searchWeatherApi(event){
+event.preventDefault();
+    console.log ("hello");
+var cityName = $("#cityname").val()
+console.log (cityName);
 }
+
+
+
+
+
+
+
+
+$("#user-form").on("submit", searchWeatherApi)
+
+
+
+
+
+
+// function geoData(lat, lon){
+// var url = `http://` ;
+// fetch (url)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+
+
+// });
+
+
+// }
