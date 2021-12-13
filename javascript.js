f769c7d39aac153622df78db2805280456a9e99f
Arya Krishna <dr.aryakris@outlook.com>
2021-12-12 23:35:04 -0800

    updated the html to introduce local storage

--- javascript.js
+++ javascript.js
@@ -8,6 +8,13 @@ var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 today = mm + '/' + dd + '/' + yyyy;
 
+var cityList = localStorage.getItem("cityList");
+if (cityList != null || cityList != undefined) {
+    for (var j = 0; j < cityList.length; j++) {
+        $("#city-list").append(cityList[j]);
+    }
+}
+
 $("#user-form").on("submit", searchWeatherApi);
 
 function searchWeatherApi(event) {
@@ -16,102 +23,115 @@ function searchWeatherApi(event) {
     var url1 = "";
     if (cityName != '') {
         var url1 = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apikey;
-    }    
+    }
     fetch(url1)
         .then(function (response) {
             return response.json();
         })
-        .then(function (currentWeather) {                      
-            console.log("currentWeather: ", currentWeather);
+        .then(function (currentWeather) {
             var lat = currentWeather.coord.lat;
             var lon = currentWeather.coord.lon;
-            var cityName = currentWeather.name; //2nd data doesn't have a name
+
             var url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apikey}&units=imperial`; //template literal
             fetch(url2)
                 .then(function (response) {
                     return response.json();
                 })
                 .then(function (forecastWeather) {
-                    console.log("forecastWeather: ", forecastWeather);
-                    // retrieving the currentweather-container html here. 
-                    var weatherIcon = $("<img>");
-                    weatherIcon.attr(
-                        "src",
-                        "https://openweathermap.org/img/w/" + currentWeather.weather[0].icon + ".png"
-                    );
-                    $("#currentweather-container").show();
-
-                    $("#current-cityname").empty();
-                    $("#current-cityname").append(cityName + "  " + today);
-
-                    $("#current-weather-icon").empty();
-                    $("#current-weather-icon").append(weatherIcon);
-                    
-                    var temp = "Temp: " + forecastWeather.current.temp +"˚F";
-                    $("#current-temperature").empty();
-                    $("#current-temperature").append(temp);
-
-                    var wind = "Wind : " + forecastWeather.current.wind_speed + "mph";
-                    $("#current-wind").empty();
-                    $("#current-wind").append(wind);
-
-                    var humidity = "Humidity :" + forecastWeather.current.humidity;
-                    $("#current-humidity").empty();
-                    $("#current-humidity").append(humidity);
-
-                    var uvIndex = "UV Index : "+ forecastWeather.current.uvi;
-                    $("#current-uv").empty();
-                    $("#current-uv").append(uvIndex);
-                    
-                    $("#fivedayweather-container").show();
-                    // var futureWeather = $("#fivedayweather-container");
-                    
-                    // get the  DAILY weather array in a variable from the data json object.
-                    var dailyWeatherArr = forecastWeather.daily;
-                    // Loop through each of the object from that array using the for loop
-                    // inside the for loop, add a div element with col class. that will be one column.
-                        
-                    for (var i = 1; i <6; i++) {
-
-                        var j = i;                     
-                        var staticDayVal = "#forcast-day-" + j+ "-";
-                        // forcast-day-01-date
-                        var htmlElementDate = staticDayVal + "date";
-
-                        // convert date from epoch to date
-                        var date = new Date(dailyWeatherArr[i].dt * 1000);
-                        var day = date.getDate();
-                        var month = date.getMonth();
-                        var year = date.getFullYear();
-                        var fullDate = day + "/" + (month + 1) + "/" + year;
-
-                        $(htmlElementDate).empty();
-                        $(htmlElementDate).append(fullDate);
-
-                        var temperature = staticDayVal + "temperature";
-                        var futureTemp = "Temp: " + dailyWeatherArr[i].temp.day + "˚F";
-                        $(temperature).empty();
-                        $(temperature).append(futureTemp);
-
-                        var hum = staticDayVal + "humidity";
-                        var futureHum = "humidity :" + dailyWeatherArr[i].humidity;
-                        $(hum).empty();
-                        $(hum).append(futureHum);
-
-                        var wind1 = staticDayVal + "wind";
-                        var futureWind = "Wind :" + dailyWeatherArr[i].wind_speed;
-                        $(wind1).empty();
-                        $(wind1).append(futureWind);
-
-                        var uvIdx = staticDayVal + "uv";
-                        var futureUvindex = "UV Index : " + dailyWeatherArr[i].uvi;
-                        $(uvIdx).empty();
-                        $(uvIdx).append(futureUvindex);
+                    // retrieve and add the new city to localstorage list
+                    var cities = JSON.parse(localStorage.getItem("cityList"));
+                    if (cities != undefined) {
+                        cities.push(cityName);
                     }
+                    else {
+                        var cities = [];
+                        cities.push(cityName);
+                    }
+                    localStorage.setItem("cityList", JSON.stringify(cities));
+
+                    // Concatenate the currentweather and forecastweather into one single json
+                    // Push the combined json to localstorage
+                    var weatherData = $.extend(currentWeather, forecastWeather);
+                    localStorage.setItem(cityName, JSON.stringify(weatherData));
+                    displayForecast(JSON.parse(localStorage.getItem(cityName)));
                 });
         })
         .catch(function (error) {
-            var errorform = $("#user-form");
-            errorform.append($("<p>").text("Enter a city name").addClass("text-danger"));
+            var errorform = $("#error-message");
+            errorform.empty();
+            errorform.append("Enter a city name");
         });
+}
+
+function displayForecast(weatherData) {
+    var cityName = weatherData.name;
+    var weatherIcon = $("<img>");
+    weatherIcon.attr(
+        "src",
+        "https://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png"
+    );
+    $("#currentweather-container").show();
+
+    $("#current-cityname").empty();
+    $("#current-cityname").append(cityName + "  " + today);
+
+    $("#current-weather-icon").empty();
+    $("#current-weather-icon").append(weatherIcon);
+
+    var temp = "Temp: " + weatherData.current.temp + "˚F";
+    $("#current-temperature").empty();
+    $("#current-temperature").append(temp);
+
+    var wind = "Wind : " + weatherData.current.wind_speed + "mph";
+    $("#current-wind").empty();
+    $("#current-wind").append(wind);
+
+    var humidity = "Humidity :" + weatherData.current.humidity;
+    $("#current-humidity").empty();
+    $("#current-humidity").append(humidity);
+
+    var uvIndex = "UV Index : " + weatherData.current.uvi;
+    $("#current-uv").empty();
+    $("#current-uv").append(uvIndex);
+
+    $("#fivedayweather-container").show();
+    // var futureWeather = $("#fivedayweather-container");
+
+    // get the  DAILY weather array in a variable from the data json object.
+    var dailyWeatherArr = weatherData.daily;
+    // Loop through each of the object from that array using the for loop
+    // inside the for loop, add a div element with col class. that will be one column.
+
+    for (var i = 1; i < 6; i++) {
+
+        var j = i;
+        var staticDayVal = "#forcast-day-" + j + "-";
+        // forcast-day-01-date
+        var htmlElementDate = staticDayVal + "date";
+
+        // convert date from epoch to date
+        var date = new Date(dailyWeatherArr[i].dt * 1000);
+        var day = date.getDate();
+        var month = date.getMonth();
+        var year = date.getFullYear();
+        var fullDate = day + "/" + (month + 1) + "/" + year;
+
+        $(htmlElementDate).empty();
+        $(htmlElementDate).append(fullDate);
+
+        var temperature = staticDayVal + "temperature";
+        var futureTemp = "Temp: " + dailyWeatherArr[i].temp.day + "˚F";
+        $(temperature).empty();
+        $(temperature).append(futureTemp);
+
+        var hum = staticDayVal + "humidity";
+        var futureHum = "humidity :" + dailyWeatherArr[i].humidity;
+        $(hum).empty();
+        $(hum).append(futureHum);
+
+        var wind1 = staticDayVal + "wind";
+        var futureWind = "Wind :" + dailyWeatherArr[i].wind_speed;
+        $(wind1).empty();
+        $(wind1).append(futureWind);
+    }
 }
